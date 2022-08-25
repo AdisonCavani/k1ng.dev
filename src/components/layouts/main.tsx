681d3d370a88b7f2810c198d7f64b7412f977c81
Adison Cavani <adriansrodon@tuta.io>
2022-08-25 13:01:11 +0200

    Update Affix, change links order

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -1,5 +1,5 @@
 import Navbar, { NavbarProps } from '@components/navbar'
-import { Affix, Button, Container, Transition } from '@mantine/core'
+import { ActionIcon, Affix, Container, Transition } from '@mantine/core'
 import { useWindowScroll } from '@mantine/hooks'
 import { IconArrowUp, IconBrandGithub } from '@tabler/icons'
 import { Router } from 'next/router'
@@ -16,13 +16,13 @@ const MainLayout = ({ children, router }: Props) => {
   const items: NavbarProps = {
     links: [
       { href: '/about', label: 'About' },
+      { href: '/404', label: '404' },
+      { href: '/500', label: '500' },
       {
         href: 'https://github.com/AdisonCavani/adisoncavani.github.io',
         label: 'Source',
         icon: <IconBrandGithub size={16} />
-      },
-      { href: '/404', label: '404' },
-      { href: '/500', label: '500' }
+      }
     ],
     path: router.asPath
   }
@@ -36,13 +36,14 @@ const MainLayout = ({ children, router }: Props) => {
       <Affix position={{ bottom: 20, right: 20 }}>
         <Transition transition="slide-up" mounted={scroll.y > 0}>
           {transitionStyles => (
-            <Button
-              leftIcon={<IconArrowUp size={16} />}
+            <ActionIcon
+              variant="filled"
+              color="blue"
               style={transitionStyles}
               onClick={() => scrollTo({ y: 0 })}
             >
-              Scroll to top
-            </Button>
+              <IconArrowUp size={16} />
+            </ActionIcon>
           )}
         </Transition>
       </Affix>
