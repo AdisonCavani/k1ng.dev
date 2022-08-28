e46464ea2566d014182c9eeffbce342cf9ff1bf7
Adison Cavani <adriansrodon@tuta.io>
2022-08-28 16:33:50 +0200

    Add hover effect to ProjectCard component

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -5,10 +5,29 @@ import {
   Badge,
   DefaultMantineColor,
   Button,
-  Stack
+  Stack,
+  createStyles
 } from '@mantine/core'
 import { IconBrandGithub } from '@tabler/icons'
 import Image from 'next/image'
+
+const useStyles = createStyles(theme => ({
+  root: {
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor:
+      theme.colorScheme === 'dark'
+        ? theme.colors.gray[8]
+        : theme.colors.gray[3],
+    transition: 'all 0.25s',
+    transitionTimingFunction: 'spring(1 100 10 10)',
+    '&:hover': {
+      transform: 'translateY(-4px)',
+      boxShadow:
+        '0 10px 15px -3px rgba(0, 0, 0, 0.1),0 4px 6px -2px rgba(0, 0, 0, 0.05)'
+    }
+  }
+}))
 
 type Props = {
   title: string
@@ -29,8 +48,10 @@ const ProjectCard = ({
   githubUrl,
   externalUrl
 }: Props) => {
+  const { classes } = useStyles()
+
   return (
-    <Card shadow="sm" p="lg" radius="md" withBorder>
+    <Card p="lg" radius="md" withBorder className={classes.root}>
       <Card.Section component="a" href={externalUrl} target="_blank">
         <Image width={600} height={280} src={img} alt="Project image" />
       </Card.Section>
