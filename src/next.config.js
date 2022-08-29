700bcb1603bf54a62946bdaf23f0c7d919fec886
Adison Cavani <adriansrodon@tuta.io>
2022-08-29 21:50:30 +0200

    Add experimental Next.js rewrites

--- src/next.config.js
+++ src/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/analytics',
+        destination: 'https://analytics.adison.me/adison.me'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
