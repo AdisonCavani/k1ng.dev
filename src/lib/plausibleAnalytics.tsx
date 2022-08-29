9b531eba1c80f2dadb94fe7fc2f816a8f8789527
Adison Cavani <adriansrodon@tuta.io>
2022-08-29 15:39:58 +0200

    Fix error during compilation

--- src/lib/plausibleAnalytics.tsx
+++ src/lib/plausibleAnalytics.tsx
@@ -27,5 +27,6 @@ export default PlausibleAnalytics
 
 // https://plausible.io/docs/custom-event-goals
 export const logEvent = (eventName: string, ...rest: any[]) => {
+  // @ts-ignore: Unreachable code error
   if (isProduction) return window.plausible?.(eventName, ...rest)
 }
