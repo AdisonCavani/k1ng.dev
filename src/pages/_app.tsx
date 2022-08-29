57c67764699b3d8b0c7b7d3a2167daa3645128a3
Adison Cavani <adriansrodon@tuta.io>
2022-08-29 15:58:00 +0200

    Fix warning in viewport meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,10 +27,7 @@ export default function App(props: AppProps) {
     <>
       <Head>
         <title>Page title</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
       <PlausibleAnalytics />
