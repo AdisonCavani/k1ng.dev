4201808583b01f6c3c7a93d708416a05c1e9b589
Adison Cavani <adriansrodon@tuta.io>
2022-08-31 21:42:04 +0200

    Replace lorem ipsum with short profile summary & image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,60 @@ import {
   SimpleGrid,
   Stack,
   Divider,
-  Center
+  Center,
+  createStyles
 } from '@mantine/core'
 import type { NextPage } from 'next'
 import ProjectCard from '@components/projectCard'
 import TechStack from '@components/techStack'
 import { PageSEO } from '@components/seo'
 import SiteMetadata from '@data/siteMetadata'
+import Image from 'next/image'
 
+import AvatarImage from '@images/avatar.webp'
 import DistroGrubThemesImage from '@images/distro-grub-themes.webp'
 
+const useStyles = createStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column-reverse',
+    alignItems: 'flex-start',
+
+    maxWidth: '42rem',
+
+    '@media (min-width: 640px)': {
+      flexDirection: 'row'
+    }
+  },
+
+  text: {
+    display: 'flex',
+    flexDirection: 'column',
+    paddingRight: '2rem'
+  },
+
+  image: {
+    position: 'relative',
+    marginBottom: '2rem',
+    marginRight: 'auto',
+    width: 80,
+
+    borderWidth: 2,
+    borderRadius: '50%',
+    borderStyle: 'solid',
+    borderColor:
+      theme.colorScheme === 'dark' ? theme.colors.gray[6] : 'transparent',
+
+    '@media (min-width: 640px)': {
+      width: 176,
+      marginBottom: 0
+    }
+  }
+}))
+
 const Home: NextPage = () => {
+  const { classes } = useStyles()
+
   return (
     <>
       <PageSEO
@@ -25,13 +68,29 @@ const Home: NextPage = () => {
       />
 
       <Container mb="xl">
-        <Stack>
-          <Title>Hello! I&apos;m Adison Cavani</Title>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget
-            luctus tortor. Nullam ultricies facilisis tempor. Phasellus eget
-            nibh mauris.
-          </Text>
+        <Stack spacing="lg">
+          <div className={classes.container}>
+            <div className={classes.text}>
+              <Title mb=".25rem">Adison Cavani</Title>
+              <Text mb="1rem">
+                Full-stack Developer, <strong>student</strong>
+              </Text>
+              <Text>
+                .NET Backend Developer. Currently learning web development -
+                cloud, serverless and React w/ Next.js.
+              </Text>
+            </div>
+            <div className={classes.image}>
+              <Image
+                src={AvatarImage}
+                alt="Adison Cavani"
+                width={176}
+                height={176}
+                sizes="30vw"
+                priority
+              />
+            </div>
+          </div>
           <PersonalTimeline />
           <Center>
             <Stack>
