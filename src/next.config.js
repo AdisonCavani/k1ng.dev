8800c3a57c7b0a4c0df44d0930ed4a1b74a3c205
Adison Cavani <adriansrodon@tuta.io>
2022-09-01 20:26:06 +0200

    Add Security Headers

--- src/next.config.js
+++ src/next.config.js
@@ -1,3 +1,27 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' analytics.adison.me;
+  connect-src 'self' analytics.adison.me;
+  style-src 'unsafe-inline';
+  font-src 'self';  
+  img-src * 'self' data: https:;
+`
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  }
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -9,6 +33,15 @@ const nextConfig = {
         source: '/analytics',
         destination: 'https://analytics.adison.me/adison.me',
         permanent: true
+      }
+    ]
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders
       }
     ]
   }
