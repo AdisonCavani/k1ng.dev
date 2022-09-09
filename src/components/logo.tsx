a8d3e966d1adda68b022a6f56880cc25c41d0e4b
Adison Cavani <adriansrodon@tuta.io>
2022-09-09 16:27:55 +0200

    Rewrite Logo in TailwindCSS

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,32 +1,15 @@
-import { ActionIcon, Text } from '@mantine/core'
 import { IconCode } from '@tabler/icons'
 import Link from 'next/link'
 
 const Logo = () => {
   return (
-    <div
-      style={{
-        cursor: 'pointer'
-      }}
-    >
+    <div className="cursor-pointer">
       <Link href="/" passHref>
-        <span
-          style={{
-            display: 'inline-flex',
-            alignItems: 'center',
-            padding: 10,
-            paddingLeft: 0
-          }}
-        >
-          <ActionIcon
-            mr="sm"
-            color="blue"
-            variant="transparent"
-            aria-label="Go to home page"
-          >
+        <span className="inline-flex items-center p-2 pl-0">
+          <div className="mr-3 text-blue-500">
             <IconCode />
-          </ActionIcon>
-          <Text weight="bold">Adison Cavani</Text>
+          </div>
+          <p className="font-bold">Adison Cavani</p>
         </span>
       </Link>
     </div>
