5f5d90470886632fbf3c75c1848a3b64fc54525a
Adison Cavani <adriansrodon@tuta.io>
2022-09-12 17:41:25 +0200

    Use dynamic imports to improve performance

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,9 @@
-import { IconMenu2 } from '@tabler/icons'
 import { Fragment, ReactNode } from 'react'
 import NextLink from 'next/link'
 import Logo from './logo'
 import ThemeButton from './themeButton'
-import { Menu, Transition } from '@headlessui/react'
+import dynamic from 'next/dynamic'
+import { Suspense } from 'react'
 
 type NavbarProps = {
   links: NavbarLinkProps[]
@@ -15,6 +15,10 @@ export type NavbarLinkProps = {
   label: string
   icon?: ReactNode
 }
+
+const MobileMenu = dynamic(() => import('./mobileMenu'), {
+  loading: () => <p>Loading...</p>
+})
 
 const Navbar = ({ links, path }: NavbarProps) => {
   const items = links.map((link, index) => {
@@ -47,33 +51,6 @@ const Navbar = ({ links, path }: NavbarProps) => {
     )
   })
 
-  const menuItems = links.map((link, index) => {
-    return (
-      <Fragment key={index}>
-        {link.href.startsWith('/') ? (
-          <Menu.Item
-            as={NextLink}
-            href={link.href}
-            className="group flex w-full items-center gap-2 rounded-md py-2 px-3 text-sm hover:bg-neutral-100 dark:hover:bg-[#383a40]"
-          >
-            {link.icon}
-            {link.label}
-          </Menu.Item>
-        ) : (
-          <Menu.Item
-            as="a"
-            href={link.href}
-            target="_blank"
-            className="group flex w-full items-center gap-2 rounded-md py-2 px-3 text-sm hover:bg-neutral-100 dark:hover:bg-[#383a40]"
-          >
-            {link.icon}
-            {link.label}
-          </Menu.Item>
-        )}
-      </Fragment>
-    )
-  })
-
   return (
     <header className="fixed top-0 left-0 right-0 z-50 h-[56px] border-b border-neutral-200 bg-[#fff]/60 shadow-sm backdrop-blur dark:border-[#2c2e33] dark:bg-[#1a1b1e]/50">
       <nav className="mx-auto flex h-[56px] max-w-4xl items-center justify-between px-4">
@@ -87,27 +64,9 @@ const Navbar = ({ links, path }: NavbarProps) => {
         <div className="flex flex-row gap-2">
           <ThemeButton />
           <div className="md:hidden">
-            <Menu>
-              <Menu.Button
-                className="rounded-md bg-neutral-200 p-2 text-neutral-700 dark:bg-neutral-800 dark:text-neutral-300"
-                aria-label="Toggle menu"
-              >
-                <IconMenu2 size={18} />
-              </Menu.Button>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items className="absolute right-0 mt-[11px] mr-[1px] w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-[#212226]">
-                  <div className="px-1 py-1">{menuItems}</div>
-                </Menu.Items>
-              </Transition>
-            </Menu>
+            <Suspense>
+              <MobileMenu links={links} />
+            </Suspense>
           </div>
         </div>
       </nav>
