1697f43bdd567be4376fd7503df34a3bb695704a
Adison Cavani <adriansrodon@tuta.io>
2022-09-20 15:46:36 +0200

    Update api/revalidate webhook

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { SanityClient } from '@lib/sanityServer'
 import { postUpdatedQuery } from '@lib/queries'
-import { isValidRequest } from '@sanity/webhook'
+import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
 
 const secret = process.env.SANITY_STUDIO_REVALIDATE_SECRET!
 
@@ -9,12 +9,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // This isn't working yet - not sure why
-  if (!isValidRequest(req, secret)) {
-    return res.status(401).json({ message: 'Invalid request' })
+  const signature = req.headers[SIGNATURE_HEADER_NAME] as string
+  const body = await readBody(req)
+
+  if (!isValidSignature(body, signature, secret)) {
+    res.status(401).json({ message: 'Invalid signature' })
+    return
   }
 
-  const { _id: id } = req.body
+  const { _id: id } = JSON.parse(req.body)
+
   if (typeof id !== 'string' || !id) {
     return res.status(400).json({ message: 'Invalid _id' })
   }
@@ -29,4 +33,12 @@ export default async function handler(
   } catch (err: any) {
     return res.status(500).json({ message: err.message })
   }
+}
+
+async function readBody(readable: NextApiRequest) {
+  const chunks = []
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
+  }
+  return Buffer.concat(chunks).toString('utf8')
 }
