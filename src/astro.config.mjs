125f94eb4e796011339e0ef102da5c42aacd57d6
Adison Cavani <adriansrodon@tuta.io>
2022-10-05 18:34:26 +0200

    Add prettier

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://adison.me",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
