8d378229c5fbb6cd72c2897a8c7d272b46252168
AdisonCavani <adriansrodon@tuta.io>
2022-10-25 13:08:18 +0200

    Fix RSS feed

--- src/src/pages/rss.xml.ts
+++ src/src/pages/rss.xml.ts
@@ -1,10 +1,18 @@
 import rss from "@astrojs/rss";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
+import { GetBlogData } from "@lib/Queries";
+import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from "../config";
+
+const data = await GetBlogData()
 
 export const get = () =>
   rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
-    items: import.meta.glob("./blog/**/*.{md,mdx}"),
+    items: data.map(post => ({
+      title: post.title,
+      description: post.description,
+      pubDate: new Date(post.datePublished),
+      link: `${SITE_URL}/blog/${post.slug}`
+    })),
   });
