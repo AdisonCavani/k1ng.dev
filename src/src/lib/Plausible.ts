547421f929c8260eb581ec7ffc0c2edf7a70e62b
Adison Cavani <adriansrodon@tuta.io>
2022-12-01 16:09:53 +0100

    Remove Astrojs project

--- src/src/lib/Plausible.ts
+++
@@ -1,7 +0,0 @@
-const isProd = import.meta.env.PROD;
-
-// https://plausible.io/docs/custom-event-goals
-export const logEvent = (eventName: string, ...rest: any[]) => {
-  // @ts-ignore: Unreachable code error
-  if (isProd) return window.plausible?.(eventName, ...rest);
-};
