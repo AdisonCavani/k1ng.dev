e4eac6345d6e7d54cfcf867bfa3d6a58978feacf
Adison Cavani <adriansrodon@tuta.io>
2022-12-03 21:39:24 +0100

    Update env variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,8 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    PUBLIC_SANITY_API_VERSION: string;
-    PUBLIC_SANITY_DATASET: string;
-    PUBLIC_SANITY_PROJECT_ID: string;
+    NEXT_PUBLIC_SANITY_API_VERSION: string;
+    NEXT_PUBLIC_SANITY_DATASET: string;
+    NEXT_PUBLIC_SANITY_PROJECT_ID: string;
     SANITY_STUDIO_REVALIDATE_SECRET: string;
     GITHUB_PAT: string;
   }
--- src/lib/sanityConfig.ts
+++ src/lib/sanityConfig.ts
@@ -1,8 +1,8 @@
 import type { ClientConfig } from "next-sanity";
 
 export const sanityConfig: ClientConfig = {
-  apiVersion: process.env.PUBLIC_SANITY_API_VERSION,
-  dataset: process.env.PUBLIC_SANITY_DATASET,
-  projectId: process.env.PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   useCdn: false,
 };
