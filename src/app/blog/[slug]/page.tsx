d88066d0ca8220cef7a353e327d1ae2975080b52
Adison Cavani <adriansrodon@tuta.io>
2022-12-05 20:39:19 +0100

    Add RSS link in blog/[slug]

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -42,9 +42,25 @@ async function BlogPost({ params: { slug } }: Props) {
         See all posts
       </Link>
       <div>
-        <time dateTime={datePublished} className="block text-sm text-slate-600">
-          Published on {formatDate(datePublished)}
-        </time>
+        <div className="inline-flex items-center text-slate-600">
+          <time dateTime={datePublished} className="block text-sm">
+            Published on {formatDate(datePublished)}
+          </time>
+          <span className="px-2 text-xs font-medium" aria-hidden={true}>
+            |
+          </span>
+          <a href="/rss.xml" target="_blank" rel="noreferrer">
+            <svg
+              className="w-4 h-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M5 3a1 1 0 000 2c5.523 0 10 4.477 10 10a1 1 0 102 0C17 8.373 11.627 3 5 3z"></path>
+              <path d="M4 9a1 1 0 011-1 7 7 0 017 7 1 1 0 11-2 0 5 5 0 00-5-5 1 1 0 01-1-1zM3 15a2 2 0 114 0 2 2 0 01-4 0z"></path>
+            </svg>
+          </a>
+        </div>
         <h1 className="mt-2 inline-block text-4xl font-extrabold leading-tight text-slate-900 lg:text-5xl">
           {title}
         </h1>
