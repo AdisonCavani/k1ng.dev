80a841c71afae21b04084d5964328e4c1bc23a1a
Adison Cavani <adriansrodon@tuta.io>
2023-06-13 15:50:55 +0200

    Sanity Studio - add required plugins and config

--- src/sanity.config.ts
+++ src/sanity.config.ts
@@ -1,24 +1,42 @@
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...index]]\page.tsx` route
- */
-
+import { colorInput } from "@sanity/color-input";
 import { visionTool } from "@sanity/vision";
-import { defineConfig } from "sanity";
+import { defineConfig, definePlugin, isDev } from "sanity";
+import { markdownSchema } from "sanity-plugin-markdown";
 import { deskTool } from "sanity/desk";
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from "./sanity/env";
+import { apiVersion, projectId } from "./sanity/env";
 import { schema } from "./sanity/schema";
 
-export default defineConfig({
-  basePath: "/studio",
-  projectId,
-  dataset,
-  // Add and edit the content schema in the './sanity/schema' folder
-  schema,
+const devOnlyPlugins = [visionTool({ defaultApiVersion: apiVersion })];
+
+const sharedConfig = definePlugin({
+  name: "shareConfig",
   plugins: [
+    colorInput(),
     deskTool(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({ defaultApiVersion: apiVersion }),
+    markdownSchema(),
+    ...(isDev ? devOnlyPlugins : []),
   ],
+
+  schema: {
+    types: schema.types,
+  },
 });
+
+export default defineConfig([
+  {
+    name: "production",
+    title: "Production",
+    projectId: projectId,
+    dataset: "production",
+    basePath: "/studio/prod",
+    plugins: [sharedConfig()],
+  },
+  {
+    name: "development",
+    title: "Development",
+    projectId: projectId,
+    dataset: "development",
+    basePath: "/studio/dev",
+    plugins: [sharedConfig()],
+  },
+]);
