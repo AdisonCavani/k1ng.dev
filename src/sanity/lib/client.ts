281601f20a8f9491e84ad8cb515ef2d255c42822
Adison Cavani <adriansrodon@tuta.io>
2023-06-23 22:32:09 +0200

    debug: sanity preview mode

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,23 +1,15 @@
 import { apiVersion, dataset, projectId, useCdn } from "@sanity/env";
 import { createClient, type SanityClient } from "next-sanity";
 
-function getClient(preview?: { token: string }): SanityClient {
+function getClient(_?: { token: string }): SanityClient {
   const client = createClient({
     projectId,
     dataset,
     apiVersion,
     useCdn,
   });
-  if (!preview) return client;
 
-  if (!preview.token)
-    throw new Error("You must provide a token to preview drafts");
-
-  return client.withConfig({
-    token: preview.token,
-    useCdn: false,
-    ignoreBrowserTokenWarning: true,
-  });
+  return client;
 }
 
 export { getClient };
